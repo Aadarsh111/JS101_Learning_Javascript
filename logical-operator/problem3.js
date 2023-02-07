62a92515943a09d44e8d0055775869404b3bc321
Aadarsh Singh <aadarshsinghbhadoriya111@gmail.com>
2023-02-07 20:01:21 +0530

    Fourth Assignments

---
+++ logical-operator/problem3.js
@@ -0,0 +1,9 @@
+let supermarket= "Sensodyne";
+
+if((supermarket=="Colgate") || (supermarket=="Closeup") || (supermarket=="Patanjali") || (supermarket=="Sensodyne"))
+{
+  console.log("I want the",supermarket);
+}
+else{
+  console.log("I do not find the toothpaste");
+}
---
+++ logical-operator/problem5.js
@@ -0,0 +1,20 @@
+let holiday= "Sunday";
+
+switch (holiday)
+  {
+    case "Monday" : console.log("No");
+      break;
+    case "Tuesday" : console.log("No");
+      break;
+    case "Wednesday" : console.log("No");
+      break;
+    case "Thursday" : console.log("No ");
+      break;
+    case "Friday" : console.log("No");
+      break;
+    case "Saturday" : console.log("No");
+      break;
+    case "Sunday" : console.log("Yes today is a holiday");
+      break;
+      deafult: console.log("invalid value");
+  }
